d8d6524d7876c3746ad7e191bf7bf96656a7ceb5
justocorneliobello <justocorneliobello@gmail.com>
2026-01-15 19:31:04 -0400

    fixed

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-  }
+  base: '/',
 })
